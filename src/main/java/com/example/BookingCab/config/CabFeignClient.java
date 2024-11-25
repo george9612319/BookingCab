7ce4dac708bbd7f55dce4733641e136330a6605a
george9612319 <130203573+george9612319@users.noreply.github.com>
2024-11-24 21:21:35 -0800

    Update CabFeignClient.java

--- src/main/java/com/example/BookingCab/config/CabFeignClient.java
+++ src/main/java/com/example/BookingCab/config/CabFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "CALCULATEFARE", url= "http://ec2-54-210-166-155.compute-1.amazonaws.com:8091")// server name in Eureka
+@FeignClient(name = "CALCULATEFARE", url= "http://ec2-54-210-166-155.compute-1.amazonaws.com:8092")// server name in Eureka
 public interface CabFeignClient {
 	
 	@GetMapping("/calculate")
